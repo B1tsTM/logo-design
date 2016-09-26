509d773b28f185f0c05fa620f13bca01a3f63ce1
Irmantas Liepis <Irmantas Liepis>
2016-09-27 00:15:44 +0300

    Fullfilled the category list and styles

--- assets/app/components/categories/categories-dashboard/categories-dashboard.component.ts
+++ assets/app/components/categories/categories-dashboard/categories-dashboard.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  templateUrl: 'categories-dashboard.component.html'
+  templateUrl: 'categories-dashboard.component.html',
+  styleUrls: ['categories-dashboard.component.css']
 })
 export class CategoriesDashboardComponent implements OnInit {
   constructor() { }
