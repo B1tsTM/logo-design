f5524539c8f67567fae63f644ce40ae4aba8f1e7
Irmantas Liepis <Irmantas Liepis>
2016-11-17 23:33:08 +0200

    Typos

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ var schema = new Schema({
   firstName: {type: String, required: true },
   lastName: {type: String, required: true },
   password: {type: String, required: true },
-  email: {type: String, required: true, unique: true },
+  email: {type: String, required: true}, //, unique: true 
   nickName: {type: String, required: true, unique: true},
   userType: {type: String, required: true},
   contestsWon: {type: Number},
--- routes/register.js
+++ routes/register.js
@@ -5,15 +5,14 @@ var passwordHash = require('password-hash');
 var User = require('../models/user');
 
 router.post('/', function(req, res, next) {
-  User.findOne({'nickName': new RegExp(req.body.nickName, "i")})
-    .exec (function(err, userDuplicate) {
-      if(err) {
+  User.findOne({'nickName': new RegExp(req.body.nickName, "i")}, function(err, dupe) {
+    if(err) {
         return res.status(404).json({
         title: 'Klaida !',
         error: err
       });
       }
-      if (res) {
+      if (dupe) {
         return res.status(404).json({
         title: 'Klaida !',
         error: err
